f1181787a68de8ea8b17e852a8ef27d0c9674b7e
Gregory Berardi <greg@berardi.us>
2022-12-31 09:23:55 -0600

    Adding simple_forecast.pl using the new NOAA_Forecast module

---
+++ simple_forecast.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl -w
+#
+
+#use lib "$ENV{HOME}/FORECAST_VANTAGE_PRO/NEW_FORECAST/NOAA_Forecast";
+use lib "/home/greg/FORECAST_VANTAGE_PRO/NEW_FORECAST/NOAA_Forecast";
+#use lib "/usr/share/perl5";
+use NOAA_Forecast;
+
+print "Content-type: text/html\n\n";
+
+my @Day; 
+my $TimeDate;
+my %forecast;
+my $snow = 0;
+
+( $Day, %forecast )  = NOAA_Forecast::GetForecast();
+
+print "<b><h3>" . $forecast{TimeDate} . "</h1></b><br>\n";
+#print @Day;
+
+foreach ( @$Day ) {
+   my $day_forecast = $forecast{$_};
+
+   if ($day_forecast=~/[S|s]now/) {
+      $snow = 1;
+   }
+   if ($snow == 1) {
+      print "<b style=\"color:blue;\">****\n";
+   }
+   else {
+      print "<b>";
+   }
+
+   # get the temp (s)
+   if ($day_forecast =~/(\d+s)/) {
+      print "<b> " . $1 . " </b><br>";
+   }
+
+   print $_ . " <===> " . "</b>" . $day_forecast . "<br>" . "\n";
+   print "<br>\n";
+
+   $snow = 0;
+}
+
